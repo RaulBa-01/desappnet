9bf41a08726d718766f96d906327f122571834f7
Raul Banuelos <so_nik8@hotmail.com>
2019-08-29 13:24:45 -0500

    envio

---
+++ 08.vectorpromedio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _08.vectorpromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vector = {
+                10,20,30,40,50,60,70,80,90,100,
+                10,20,30,40,50,60,70,80,90,100,
+                10,20,30,40,50,60,70,80,90,100,
+                10,20,30,40,50,60,70,80,90,100,
+            };
+
+            int suma=0, nmp=0;
+            float promedio=0;
+
+            Console.Write("los elementos del arreglo son: \n");
+            for(int i=0; i<vector.Length; i++){
+                Console.Write($"{vector[i]} ");
+                suma += vector[i];  
+            
+            }
+            promedio = suma / vector.Length;
+            Console.WriteLine($"\n el promedio es {promedio}\n");
+            
+            Console.Write("numeros mayores que el promedio: \n");
+            for(int i=0; i<vector.Length; i++){
+                if(vector[i]>promedio) {
+                    Console.Write($"{vector[i]} ");
+                    nmp++;
+                }
+             }
+            
+        
+            
+        }
+    }
+}
---
+++ 09.vectoresaleatorios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09.vectoresaleatorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] A = new int[15];
+            int [] B = new int[15];
+            int [] C = new int[15];
+            Random rnd = new Random();
+
+            for(int i=0; i<A.Length; i++) {
+                A[i]= rnd.Next(1,30);
+                B[i]= rnd.Next(1,30);
+                C[i]=  A[i]+B[i];
+            }
+
+            Console.WriteLine("\n el vector A" ); imprime(A);
+            Console.WriteLine("\n el vector B" ); imprime(B);
+            Console.WriteLine("\n el vector C" ); imprime(C);
+
+            
+        }
+        static void imprime(int [] v) {
+            for(int i=1; i<v.Length; i++)
+            Console.Write($"{v[i]}");
+        }
+    }
+}
